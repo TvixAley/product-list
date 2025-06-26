f722f1350b8d9d1177d60e36da1d59cdbf99a6cb
Bryukhanov Daniil <jenikinov747@gmail.com>
2025-06-26 19:24:24 +0300

    fixed fetch "data.json"

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ const activeCartElement = document.querySelector(".active-cart")
 const productCart = []
 
 
-fetch("../../data.json")
+fetch("data.json")
   .then(response => {
     if (!response.ok) {
       throw new Error('data.json loading error');
